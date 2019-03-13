0c12e5b6c7816bac0bdaa38c433b0c2571234bfb
Michal Nikoniuk <mnikoniuk97@gmail.com>
2019-03-13 20:56:12 +0100

    PortfolioFirstVersion

---
+++ jsfile.js
@@ -0,0 +1,29 @@
+
+$(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+$(function () {
+    $('.page-scroll a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+//----------------------------
+ 
+function showAboutMe(){
+
+     document.getElementById("diva").style.display = "";
+}
+
+function hideAboutMe(){
+     document.getElementById("diva").style.display = "none";
+}
